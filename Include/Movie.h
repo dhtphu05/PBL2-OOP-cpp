3a3540ab6cb98cdf32cbc3367a19681509614d9d
Doan Hoang Thien Phu <dhtphu05@gmail.com>
2024-09-25 15:21:56 +0700

    init directories structure

---
+++ Include/Movie.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Movie{
+    private:
+    string ID_Movie;
+    string title;
+    string genre;
+    string releaseDate;
+    string director;
+    string rating;
+    string duration;
+    string actor;
+    string language;
+    string description;
+    
+
+
+
+
+};
